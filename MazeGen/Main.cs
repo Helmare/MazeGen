899c81b3c6d9b4a24bab1904190851339c553ee0
Hazdryx <hazdryx@gmail.com>
2021-03-20 11:10:48 -0500

    Setup main form.

--- MazeGen/Main.cs
+++ MazeGen/Main.cs
@@ -10,12 +10,11 @@ using System.Windows.Forms;
 
 namespace MazeGen
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
-
     }
 }
